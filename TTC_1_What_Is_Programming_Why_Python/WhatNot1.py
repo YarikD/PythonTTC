54fb1c3ed00a684ec2fad6f102e1fc46cd65910f
Yarik1977 <yaroslav.denysenkov@gmail.com>
2017-06-05 12:22:05 +0300

    TTC_2_Variables_Operations_and_Input_Output

--- TTC_1_What_Is_Programming_Why_Python/WhatNot1.py
+++ TTC_1_What_Is_Programming_Why_Python/WhatNot1.py
@@ -2,3 +2,6 @@ print(10 % 3)
 print(10 // 3)
 print(10 / 3)
 print(input("print me:")*3)  # N.B. prints the input 3 times
+print("test "*3)  # ok
+print("test "+3)  # rte
+print("test "/3)  # rte
---
+++ TTC_2_Variables_Operations_and_Input_Output/WhatNot2.py
@@ -0,0 +1,26 @@
+x = 3
+y = 3.14
+z = "Make pizza"
+w = 'Make pizza'
+v = z + w
+print(x, y, '\n', z, w, '\n', v)
+
+#  print(input(":")+input(":"))  # string + string
+#  print(int(input(":"))+int(input(":")))  # int + int
+#  print(float(input(":"))+float(input(":")))  # float + float
+
+a = 10
+a += 5
+a *= 10
+print(float(a))
+
+s1 = "Welcome"
+s2 = "Home"
+
+print(s1, s2)  # Welcome Home
+print(s1+s2)  # WelcomeHome
+
+print('-'*20)
+name = input("name: ")
+characteristics = input("characteristics: ")
+print("Sir", name, "the", characteristics)
